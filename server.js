7522444a614950992ff4cf8d02b7932a07536771
p00149021@houstonisd.org Mag17615@7 <lioneljones5116@gmail.com>
2022-10-26 18:07:16 -0500

    Adding to Heroku again 2

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/api/receipt', require('./routes/api/receipt'));
 if(process.env.NODE_ENV === 'production')
 {
     //Set static folder (our public folder) - dist = VUE JS/ build = react
-    app.use(express.static('client/dist'));
+    app.use(express.static('client/build'));
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html')); 
     })
