1eeb59cf41a6b69fe3a86f718f1d40fe6848838d
Jones <P00149021@houstonisd.org>
2022-10-26 08:40:29 -0500

    Version 2 Complete.

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,25 @@ import { Route,HashRouter } from 'react-router-dom'
 import Login from './components/Login/Login'
 import Alert from './components/reusable/Alert';
 import Register from './components/Login/Register';
+import test from './components/Login/test';
 
 import Pharma from './components/Pharama/Pharma'
 import SearchPharma from './components/Search/SearchPharma'
+
+import MedicalGroup from './components/MedicalGroup/MedicalGroup';
+import SearchMedicalGroup from './components/Search/SearchMedicalGroup';
+
+import Physicians from './components/Physicians/Physicians';
+import SearchPhysicians from './components/Search/SearchPhysicians';
+
+import Manager from './components/Manager/Manager';
+import SearchManager from './components/Search/SearchManager';
+
+import Rep from './components/Rep/Rep';
+import SearchRep from './components/Search/SearchRep';
+
+import Receipt from './components/Receipt/Receipt';
+import SearchReceipt from './components/Search/SearchReceipt';
 
 function App() {
   return (
@@ -42,7 +58,69 @@ function App() {
             exact
             path='/SearchPharma'
             component={SearchPharma}
+          />  
+       <Route
+            exact
+            path='/MedicalGroup'
+            component={MedicalGroup}
           />
+           <Route
+            exact
+            path='/SearchMedicalGroup'
+            component={SearchMedicalGroup}
+            />
+
+          <Route
+            exact
+            path='/Physicians'
+            component={Physicians}
+          />
+           <Route
+            exact
+            path='/SearchPhysicians'
+            component={SearchPhysicians}
+          />
+
+         <Route
+            exact
+            path='/Manager'
+            component={Manager}
+          />
+           <Route
+            exact
+            path='/SearchManager'
+            component={SearchManager}
+          />
+
+        <Route
+            exact
+            path='/Rep'
+            component={Rep}
+          />
+           <Route
+            exact
+            path='/SearchRep'
+            component={SearchRep}
+          />
+
+         <Route
+            exact
+            path='/Receipt'
+            component={Receipt}
+          />
+           <Route
+            exact
+            path='/SearchReceipt'
+            component={SearchReceipt}
+          />
+
+          <Route
+            exact
+            path='/test'
+            component={test}
+          />
+          
+
       </HashRouter>
       
     </div>
